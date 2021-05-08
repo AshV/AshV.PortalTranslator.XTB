c38435a52f23a5c12e188f0c4c6f88a66f43782c
AshV <ashish6318@gmail.com>
2021-05-09 01:14:35 +0530

    language code formatting

--- TranslationHelpers/GoogleTranslator.cs
+++ TranslationHelpers/GoogleTranslator.cs
@@ -1,4 +1,6 @@
-﻿using Google.Cloud.Translate.V3;
+﻿using Google.Api.Gax.ResourceNames;
+using Google.Cloud.Translate.V3;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +11,45 @@ namespace AshV.PortalTranslator.XTB.TranslationHelpers
 {
     partial class GoogleTranslator : ITranslator
     {
+        string projectId;
+        TranslationServiceClient translationServiceClient;
         public GoogleTranslator()
         {
-            TranslationServiceClientBuilder s = new TranslationServiceClientBuilder();
-            s.JsonCredentials = Key;
-            var client = s.Build();
+            TranslationServiceClientBuilder translationServiceClientBuilder = new TranslationServiceClientBuilder();
+            translationServiceClientBuilder.JsonCredentials = Key;
+            projectId = JsonConvert.DeserializeObject<GKey>(Key).project_id;
+            translationServiceClient = translationServiceClientBuilder.Build();
         }
 
         public TranslationResult GetTranslation(TranslationRequest translationRequest)
         {
-            throw new NotImplementedException();
+            translationRequest.TargetLangauges.ToList().ForEach(lang =>
+            {
+                TranslateTextRequest request = new TranslateTextRequest
+                {
+                    Contents = { "It is raining." },
+                    TargetLanguageCode = "fr",
+                    Parent = new ProjectName(projectId).ToString(),
+                    
+                };
+
+                TranslateTextResponse response = translationServiceClient.TranslateText(request);
+                // response.Translations will have one entry, because request.Contents has one entry.
+                Translation translation = response.Translations[0];
+                Console.WriteLine($"Detected language: {translation.DetectedLanguageCode}");
+                Console.WriteLine($"Translated text: {translation.TranslatedText}");
+
+            });
+            return new TranslationResult
+            {
+            };
         }
     }
+
+
+    public class GKey
+    {
+        public string project_id { get; set; }
+    }
+
 }
